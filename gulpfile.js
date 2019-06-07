5a7a3fd7982217964b98722467139865966e7c37
Senseytreyser <senseytreyser@mail.ru>
2019-06-07 13:11:05 +0300

    Первые шаги

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+﻿'use strict'
+
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+
+gulp.task("css",function hello(callback){
+    return gulp.src('frontend/**/*.scss')
+        .pipe(sass())
+        .pipe(concat('slyle.css'))
+        .pipe(gulp.dest('product'));
+});
