5101cd91166cd50af7c230771c3d07e4b5d604a4
Senseytreyser <senseytreyser@mail.ru>
2019-06-11 13:47:43 +0300

    Scss in css

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,22 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const sass = require('gulp-sass');
+const del = require('del');
 
-gulp.task("css",function hello(callback){
+gulp.task("css",function(){
     return gulp.src('frontend/**/*.scss')
         .pipe(sass())
-        .pipe(concat('slyle.css'))
+        .pipe(concat('style.css'))
         .pipe(gulp.dest('product'));
 });
+
+gulp.task("clear", function(){
+    return del('product');
+});
+
+gulp.task("assets", function(){
+    return gulp.src('frontend/assets/**')
+        .pipe(gulp.dest('product'));
+});
+
+gulp.task('build', gulp.series('clear', gulp.parallel('css','assets')))
